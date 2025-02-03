fabe5a41f8bd31e7a2431784f78e3700c676b9d8
leonardomedd <leonardomedd@gmail.com>
2025-02-03 17:36:06 -0300

    Preparar para deploy no Elastic Beanstalk

--- ProjectManagementAPI/obj/Debug/net9.0/ProjectManagementAPI.AssemblyInfo.cs
+++ ProjectManagementAPI/obj/Debug/net9.0/ProjectManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d60d199cd917a9e2544d269a79645cee40483ca8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
